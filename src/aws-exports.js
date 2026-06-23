6088fd7dba0f06244a44784a3fb505c604ac0ed3
Roland Lucky Brown <brownroland27@gmail.com>
2026-06-23 18:15:25 +0000

    added aws cognito auth wrapper

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+// src/aws-exports.js
+export const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_d0QAFzQcy",
+  client_id: "1o102dakja9qanr0cj4djuleae",
+  redirect_uri: "https://d84lly8p4kdic.cloudfront.net/",
+  response_type: "code",
+  scope: "phone openid email",
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from 'react-oidc-context';
+import { cognitoAuthConfig } from './aws-exports';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
